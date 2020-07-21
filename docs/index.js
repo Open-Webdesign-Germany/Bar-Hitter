b567ee00844d2d894e19c60d031e6e22fc93f967
OWDG <62072464+OW-DG@users.noreply.github.com>
2020-07-21 14:58:56 +0200

    Updated background picture
    Co-Authored-By: OWDG <62072464+ow-dg@users.noreply.github.com>

--- docs/index.js
+++ docs/index.js
@@ -29,9 +29,9 @@ window.addEventListener('beforeinstallprompt', (e) => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then((choiceResult) => {
         if (choiceResult.outcome === 'accepted') {
-          console.log('User accepted the A2HS prompt');
+          console.log('User accepted the install prompt');
         } else {
-          console.log('User dismissed the A2HS prompt');
+          console.log('User dismissed the install prompt');
         }
         deferredPrompt = null;
       });
